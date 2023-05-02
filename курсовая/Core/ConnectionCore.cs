4077a99b765de48d2a6f03c6d043b8836f617191
ASRock <ASRock@ASROCK-PC92>
2023-05-02 20:56:56 +0300

    Добавьте файлы проекта.

---
+++ курсовая/Core/ConnectionCore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace курсовая.Core
+{
+    public static class ConnectionCore
+    {
+
+
+        public static Frame? CoreFrame { get; set; }
+    }
+}
